6773a526415a61026b149b7642f7c3e78f46ef8b
tallgy <1776232657@qq.com>
2021-01-15 09:14:46 +0800

    begin

---
+++ src/components/mask/mask.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<view class=".cpt_mask"></view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.cpt_mask {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: #000000;
+		opacity: 0.5;
+		z-index: 99;
+	}
+</style>
---
+++ src/pages/person/person.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		个人
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
