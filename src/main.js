8921fc1dabc718d379fb7b23027ee2965f1218ed
tallgy <1776232657@qq.com>
2021-01-17 13:27:19 +0800

    个人信息的修改，前后端交互的开始

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App'
 
 Vue.config.productionTip = false
 
-Vue.prototype.$api='http://192.168.2.114:9999'
+Vue.prototype.$api = 'http://172.20.10.4:9092'
+// Vue.prototype.$token = null;
+// Vue.prototype.$right = null;
 
 App.mpType = 'app'
 
